4727ec104da16c2a21544710068a87e4bd06686c
ehaupt04 <ehaupt04@gmail.com>
2023-01-24 09:42:51 -0600

    made a comment in intellij

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args)
     {
+        System.out.println("yeehaw");
       System.out.println("made a comment in replit.");
         JFrame myFrame = new JFrame();
         myFrame.setSize(500,500);
